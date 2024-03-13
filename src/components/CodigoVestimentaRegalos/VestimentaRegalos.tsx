ca352e3dc02f15d8ef960bdd78b10ad1334048ca
ivanPepe <ji.perez@dataware.com.mx>
2024-03-12 23:36:30 -0600

    Update UI
    Update UI , add dress code and regalos

--- src/components/CodigoVestimentaRegalos/VestimentaRegalos.tsx
+++ src/components/CodigoVestimentaRegalos/VestimentaRegalos.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 
 export const VestimentaRegalos = () => {
   return (
@@ -11,8 +10,19 @@ export const VestimentaRegalos = () => {
             <img src="../src/assets/dresscodemen.png" alt="dressmen" />
             <img src="../src/assets/dresscodegirl.png" alt="dressgirl" />
           </div>
+          <br />
+          <h1 className="principal_dresscode_h1">Restricciones</h1>
+          <p className="principal_dresscode_p">Favor de no usar ropa con ningun tono color 🔵</p>
         </section>
-        <section className="principal_regalo"></section>
+        <section className="principal_regalo">
+          <h1 className="principal_regalo_h1">Regalos</h1>
+          <p className="principal_regalo_p">
+            El regalo es opcional, pero si 
+            quieres tener un detalle conmigo me podrias dar un apoyo en efectivo dentro de un sobre cerrado.
+          </p>
+          
+          <img src="../src/assets/sobre.png" alt="SobreX"  className='regalo_img'/>
+        </section>
       </section>
     </section>
   )
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { createTheme, MantineProvider } from '@mantine/core';
+import { MantineProvider } from '@mantine/core';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <MantineProvider>
