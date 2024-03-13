7415da0ae1da609301aa2497e947097202585a61
ivanPepe <ji.perez@dataware.com.mx>
2024-03-13 17:35:36 -0600

    Update UI
    Update UI add new icon to dress code

--- src/components/CodigoVestimentaRegalos/VestimentaRegalos.tsx
+++ src/components/CodigoVestimentaRegalos/VestimentaRegalos.tsx
@@ -7,21 +7,19 @@ export const VestimentaRegalos = () => {
           <h1 className="principal_dresscode_h1">Dress Code</h1>
           <p className="principal_dresscode_p">Formal</p>
           <div className="principal_images">
-            <img src="../src/assets/dresscodemen.png" alt="dressmen" />
-            <img src="../src/assets/dresscodegirl.png" alt="dressgirl" />
+            
           </div>
           <br />
           <h1 className="principal_dresscode_h1">Restricciones</h1>
-          <p className="principal_dresscode_p">Favor de no usar ropa con ningun tono color 🔵</p>
+          <p className="principal_dresscode_p">Favor de no usar ropa con ningun tono color Azul 🔵</p>
         </section>
         <section className="principal_regalo">
           <h1 className="principal_regalo_h1">Regalos</h1>
           <p className="principal_regalo_p">
             El regalo es opcional, pero si 
             quieres tener un detalle conmigo me podrias dar un apoyo en efectivo dentro de un sobre cerrado.
-          </p>
-          
-          <img src="../src/assets/sobre.png" alt="SobreX"  className='regalo_img'/>
+          </p>          
+          <div className='regalo_img'></div>
         </section>
       </section>
     </section>
